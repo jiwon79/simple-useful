cbcd112c0d566b3c7092c872deb5d5a973bddbe7
jiwon79 <19-079@ksa.hs.kr>
2022-12-18 06:13:22 +0900

    :sparkles: mock Card 추가

---
+++ app/math/remain/page.tsx
@@ -0,0 +1,9 @@
+const RemainPage = () => {
+  return (
+    <div>
+      <h1>Remain</h1>
+    </div>
+  )
+}
+
+export default RemainPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import Card from "component/main/Card/Card";
+import CardGroup from "../component/main/CardGroup/CardGroup";
+import { CardProps } from "component/main/Card/Card";
 
 export default function Home() {
+  const mathCards: CardProps[] = [
+    { name: "Remain-1", desc: "Remain desc - 1", href: "/math/remain" },
+    { name: "Remain-1", desc: "Remain desc - 1", href: "/math/remain" },
+    { name: "Remain-1", desc: "Remain desc - 1", href: "/math/remain" },
+    { name: "Remain-1", desc: "Remain desc - 1", href: "/math/remain" },
+  ];
+
+  const ClockCards: CardProps[] = [
+    { name: "Clock-1", desc: "Remain desc - 1", href: "/math/remain" },
+    { name: "Clock-1", desc: "Remain desc - 1", href: "/math/remain" },
+    { name: "Remain-1", desc: "Remain desc - 1", href: "/math/remain" },
+    { name: "Remain-1", desc: "Remain desc - 1", href: "/math/remain" },
+  ];
+
   return (
     <>
-      <Card />
+      <CardGroup title={"MATH"} icon={"/icon/calculate_black_24dp.svg"} cards={mathCards} />
+      <CardGroup title={"CLOCK"} icon={"/icon/clock_black.svg"} cards={ClockCards} />
     </>
   )
 }
