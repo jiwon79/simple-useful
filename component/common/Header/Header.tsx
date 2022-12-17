27dfde7c4ab342b393452944723e93339c91a1fa
jiwon79 <19-079@ksa.hs.kr>
2022-12-18 02:31:53 +0900

    :lipstick: Header style 추가

--- component/common/Header/Header.tsx
+++ component/common/Header/Header.tsx
@@ -1,8 +1,39 @@
+import Image from 'next/image'
+import styles from './Header.module.scss';
+
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className={styles.header}>
+      <div>
+        <Image
+          src="/favicon.png"
+          width={32}
+          height={32}
+          alt={"web logo"}
+          className={styles.logo}
+        />
+        <p className={styles.title}>Simple & Useful</p>
+      </div>
+
+      <div>
+        <div className={styles.icon}>
+          <Image
+            src={"/icon/dark_mode_black_24dp.svg"}
+            width={24}
+            height={24}
+            alt={"dark mode"}
+          />
+        </div>
+        <div className={styles.icon}>
+          <Image
+            src={"/icon/search_black_24dp.svg"}
+            width={24}
+            height={24}
+            alt={"search icon"}
+          />
+        </div>
+      </div>
+    </header>
   )
 }
 
