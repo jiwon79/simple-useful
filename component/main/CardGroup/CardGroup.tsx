66ed7a5678cb98323417d1f892ed192860f65290
jiwon79 <19-079@ksa.hs.kr>
2022-12-18 06:12:48 +0900

    :lipstick: CardGroup 스타일 추가

---
+++ component/main/CardGroup/CardGroup.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Card, { CardProps } from "../Card/Card";
+import styles from "./CardGroup.module.scss";
+
+export interface CardGroupProps {
+  title: string;
+  icon: string;
+  cards: CardProps[];
+}
+
+const CardGroup = ({ title, icon, cards }: CardGroupProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <Image
+          src={icon}
+          width={32}
+          height={32}
+          alt={"web logo"}
+          className={styles.icon}
+        />
+        <p className={styles.title}>
+          {title}
+        </p>
+      </div>
+      <div className={styles.card__wrapper}>
+        {cards.map((card) => (
+          <Card {...card} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardGroup;
+
