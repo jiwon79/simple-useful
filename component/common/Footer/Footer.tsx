b5457cd298301a64f2d0aec578e7468f668081bd
jiwon79 <19-079@ksa.hs.kr>
2023-10-03 20:31:14 +0900

    :lipstick: header, footer 스타일

--- component/common/Footer/Footer.tsx
+++ component/common/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>SIMPLE & USEFUL WEBSITE</p>
-      <p>&copy; 2022. Jiwon all rights reserved.</p>
+      <p>&copy; 2023. Jiwon all rights reserved.</p>
       <p>CONTACT US : jiiwon79@gmail.com</p>
     </footer>
   )
