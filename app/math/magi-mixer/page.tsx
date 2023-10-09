516370d8797683ef8abbb2c0e738bd72354d4d39
jiwon79 <19-079@ksa.hs.kr>
2023-10-09 18:50:25 +0900

    :sparkles: Math button, editor 연결

--- app/math/magi-mixer/page.tsx
+++ app/math/magi-mixer/page.tsx
@@ -1,12 +1,54 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { MathField } from "react-mathquill";
+import { MathButton, MathEditor } from "@/component/math/magi-mixer";
 
 const MagiMixerPage = () => {
+  const mathFieldRef = useRef<MathField>();
   const [value, setValue] = useState<string>('');
+
+  const handleMathField = (mathField: MathField) => {
+    mathFieldRef.current = mathField
+    setValue(mathField.latex())
+  }
+
+  const cmd = (cmd: string) => {
+    if (mathFieldRef.current === undefined) return;
+    mathFieldRef.current.cmd(cmd)
+    mathFieldRef.current.focus();
+    setValue(mathFieldRef.current.latex() ?? '')
+  }
+
+  const keystroke = (key: string) => {
+    if (mathFieldRef.current === undefined) return;
+    mathFieldRef.current.focus();
+    mathFieldRef.current.keystroke(key)
+    setValue(mathFieldRef.current.latex() ?? '')
+  }
 
   return (
     <>
-      <p>aa</p>
+      <MathEditor mathField={mathFieldRef.current} handleMathField={handleMathField}/>
+      <p>{value}</p>
+      <MathButton onClick={() => cmd("1")} text={"1"}/>
+      <MathButton onClick={() => cmd("2")} text={"2"}/>
+      <MathButton onClick={() => cmd("3")} text={"3"}/>
+      <MathButton onClick={() => cmd("4")} text={"4"}/>
+      <MathButton onClick={() => cmd("5")} text={"5"}/>
+      <MathButton onClick={() => cmd("6")} text={"6"}/>
+      <MathButton onClick={() => keystroke("Left")} text={"<"}/>
+      <MathButton onClick={() => keystroke("Right")} text={">"}/>
+      <MathButton onClick={() => cmd("\\sqrt")} text={"√"}/>
+      <MathButton onClick={() => cmd("+")} text={"+"}/>
+      <MathButton onClick={() => cmd("-")} text={"-"}/>
+      <MathButton onClick={() => cmd("*")} text={"*"}/>
+      <MathButton onClick={() => cmd("/")} text={"/"}/>
+      <MathButton onClick={() => cmd("\\left")} text={"("}/>
+      <MathButton onClick={() => cmd("\\right")} text={")"}/>
+      <MathButton onClick={() => cmd("\\lfloor")} text={"⌊"}/>
+      <MathButton onClick={() => cmd("\\rfloor")} text={"⌋"}/>
+      <MathButton onClick={() => cmd("\\lceil")} text={"⌈"}/>
+      <MathButton onClick={() => cmd("\\rceil")} text={"⌉"}/>
     </>
   )
 }
