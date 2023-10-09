ef18962407d6e30b6914cce3f77c75b94104d66a
jiwon79 <19-079@ksa.hs.kr>
2023-10-09 21:39:51 +0900

    :sparkles: 숫자 계산 기능 추가

--- app/math/magi-mixer/page.tsx
+++ app/math/magi-mixer/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MathField } from "react-mathquill";
 import { NumberGroup, MathEditor, MathButtonGroup } from "@/component/math/magi-mixer";
+import { evaluate } from "mathjs";
 
 const MagiMixerPage = () => {
   const mathFieldRef = useRef<MathField>();
@@ -26,14 +27,50 @@ const MagiMixerPage = () => {
     setValue(mathFieldRef.current.latex() ?? '')
   }
 
+  useEffect(() => {
+    const result = evaluateLatexExpression(value)
+  }, [value])
+
   return (
     <>
       <NumberGroup inputs={[1, 2, 3, 4, 5]} outputs={[3, 6]} used={[2, 2, 3]}/>
-      <MathEditor mathField={mathFieldRef.current} handleMathField={handleMathField}/>
+      <div>
+        <MathEditor
+          mathField={mathFieldRef.current}
+          handleMathField={handleMathField}
+        />
+        <p>=</p>
+        <p>1</p>
+
+      </div>
       <p>{value}</p>
       <MathButtonGroup cmd={cmd} keystroke={keystroke}/>
     </>
   )
+}
+
+function evaluateLatexExpression(expression): { result: number, error: boolean, errorMsg: string } {
+  if (expression.trim() === '') return {result: 0, error: false, errorMsg: ''};
+
+  const continueNumberRegex = /\d{2}/;
+  const isContinueNumber = continueNumberRegex.test(expression);
+  if (isContinueNumber) return {result: 0, error: true, errorMsg: '숫자를 연속으로 사용할 수 없습니다.'};
+
+  const mathExpression = expression
+    .replace(/\\lceil(.*?)\\rceil/g, 'ceil($1)')
+    .replace(/\\lfloor(.*?)\\rfloor/g, 'floor($1)')
+    .replace(/\\sqrt{(.*?)}/g, 'sqrt($1)')
+    .replace(/\\frac{(.*?)}{(.*?)}/g, '($1)/($2)')
+    .replace(/\\cdot/g, '*')
+    .replace(/\\left/g, '(')
+    .replace(/\\right/g, ')')
+
+  try {
+    const result = evaluate(mathExpression);
+    return {result, error: false, errorMsg: ''};
+  } catch (e) {
+    return {result: 0, error: true, errorMsg: '잘못된 수식입니다.'}
+  }
 }
 
 export default MagiMixerPage
