1cf914ae6b430ac37f7ab2e7af37e976202c49b0
jiwon79 <19-079@ksa.hs.kr>
2023-10-09 19:10:32 +0900

    :sparkles: MathButtonGroup Row로 구분

--- component/math/magi-mixer/MathButtonGroup.tsx
+++ component/math/magi-mixer/MathButtonGroup.tsx
@@ -9,29 +9,44 @@ interface MathButtonWrapProps {
 export const MathButtonGroup = ({cmd, keystroke}: MathButtonWrapProps) => {
   return (
     <Group>
-      <MathButton onClick={() => cmd("1")} text={"1"}/>
-      <MathButton onClick={() => cmd("2")} text={"2"}/>
-      <MathButton onClick={() => cmd("3")} text={"3"}/>
-      <MathButton onClick={() => cmd("4")} text={"4"}/>
-      <MathButton onClick={() => cmd("5")} text={"5"}/>
-      <MathButton onClick={() => cmd("6")} text={"6"}/>
-      <MathButton onClick={() => keystroke("Left")} text={"<"}/>
-      <MathButton onClick={() => keystroke("Right")} text={">"}/>
-      <MathButton onClick={() => cmd("\\sqrt")} text={"√"}/>
-      <MathButton onClick={() => cmd("+")} text={"+"}/>
-      <MathButton onClick={() => cmd("-")} text={"-"}/>
-      <MathButton onClick={() => cmd("*")} text={"*"}/>
-      <MathButton onClick={() => cmd("/")} text={"/"}/>
-      <MathButton onClick={() => cmd("\\left")} text={"("}/>
-      <MathButton onClick={() => cmd("\\right")} text={")"}/>
-      <MathButton onClick={() => cmd("\\lfloor")} text={"⌊"}/>
-      <MathButton onClick={() => cmd("\\rfloor")} text={"⌋"}/>
-      <MathButton onClick={() => cmd("\\lceil")} text={"⌈"}/>
-      <MathButton onClick={() => cmd("\\rceil")} text={"⌉"}/>
+      <Row>
+        <MathButton onClick={() => cmd("1")} text={"1"}/>
+        <MathButton onClick={() => cmd("2")} text={"2"}/>
+        <MathButton onClick={() => cmd("3")} text={"3"}/>
+        <MathButton onClick={() => cmd("4")} text={"4"}/>
+        <MathButton onClick={() => cmd("5")} text={"5"}/>
+        <MathButton onClick={() => cmd("6")} text={"6"}/>
+      </Row>
+      <Row>
+        <MathButton onClick={() => cmd("+")} text={"+"}/>
+        <MathButton onClick={() => cmd("-")} text={"-"}/>
+        <MathButton onClick={() => cmd("*")} text={"*"}/>
+        <MathButton onClick={() => cmd("/")} text={"/"}/>
+        <MathButton onClick={() => cmd("\\sqrt")} text={"√"}/>
+      </Row>
+      <Row>
+        <MathButton onClick={() => cmd("\\left")} text={"("}/>
+        <MathButton onClick={() => cmd("\\right")} text={")"}/>
+        <MathButton onClick={() => cmd("\\lfloor")} text={"⌊"}/>
+        <MathButton onClick={() => cmd("\\rfloor")} text={"⌋"}/>
+        <MathButton onClick={() => cmd("\\lceil")} text={"⌈"}/>
+        <MathButton onClick={() => cmd("\\rceil")} text={"⌉"}/>
+      </Row>
+      <Row>
+        <MathButton onClick={() => keystroke("Up")} text={"↑"}/>
+        <MathButton onClick={() => keystroke("Down")} text={"↓"}/>
+        <MathButton onClick={() => keystroke("Left")} text={"←"}/>
+        <MathButton onClick={() => keystroke("Right")} text={"→"}/>
+      </Row>
     </Group>
   );
 }
 
 const Group = styled("div", {
   display: "flex",
+  flexDirection: "column",
 })
+
+const Row = styled("div", {
+  display: "flex",
+});
