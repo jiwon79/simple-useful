f2ece878f0ca643c18128f9b8ec726a424503b69
jiwon79 <19-079@ksa.hs.kr>
2023-10-11 02:04:50 +0900

    :heavy_plus_sign: react-toastify 추가

--- lib/views/game/magi-mixer/page/page.tsx
+++ lib/views/game/magi-mixer/page/page.tsx
@@ -1,3 +1,4 @@
+import { toast } from 'react-toastify';
 import { NumberGroup, MathEditor, MathButtonGroup } from "../components";
 import { useEvaluateExpression, useMathField } from "../hooks";
 
@@ -5,8 +6,12 @@ export const MagiMixerPage = () => {
   const {latex, mathField, handleMathField, cmd, keystroke} = useMathField();
   const {used, result, error, errorMsg} = useEvaluateExpression(latex);
 
+  const notify = () => toast("Wow so easy!", {
+    type: "error"
+  });
   return (
     <>
+      <button onClick={() => notify()}>button</button>
       <NumberGroup inputs={[1, 2, 3, 4, 5]} outputs={[3, 6]} used={used}/>
       <div>
         <MathEditor
