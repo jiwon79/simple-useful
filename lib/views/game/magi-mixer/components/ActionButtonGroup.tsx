df4c5bbea3f78f2335c1532050d199db79a3f104
jiwon79 <19-079@ksa.hs.kr>
2023-10-15 02:22:47 +0900

    :lipstick: ActionButtonGroup 스타일 추가

--- lib/views/game/magi-mixer/components/ActionButtonGroup.tsx
+++ lib/views/game/magi-mixer/components/ActionButtonGroup.tsx
@@ -1,3 +1,5 @@
+import { styled } from "@/stitches.config";
+
 interface ActionButtonGroupProps {
   onSuccess: () => Promise<void>;
   onFail: () => Promise<void>;
@@ -7,10 +9,38 @@ interface ActionButtonGroupProps {
 
 export const ActionButtonGroup = ({onSuccess, onFail, reset, isSuccess}: ActionButtonGroupProps) => {
   return (
-    <div>
-      <button onClick={onSuccess} disabled={!isSuccess}>제출하기</button>
-      <button onClick={onFail}>불가능</button>
-      <button onClick={reset}>숫자 바꾸기</button>
-    </div>
+    <Wrap>
+      <Button type="primary" onClick={onSuccess} disabled={isSuccess}>제출하기</Button>
+      <Button type="secondary" onClick={onFail}>불가능</Button>
+      <Button type="secondary" onClick={reset}>숫자 바꾸기</Button>
+    </Wrap>
   )
 }
+
+const Wrap = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  gap: "20px",
+  margin: "16px 0",
+});
+
+const Button = styled("button", {
+  height: "36px",
+  width: "100px",
+  paragraph_14: true,
+  border: "none",
+  borderRadius: "4px",
+  cursor: "pointer",
+
+  variants: {
+    type: {
+      primary: {
+        backgroundColor: "$main300",
+      },
+      secondary: {
+        backgroundColor: "$grey200",
+        border: "1px solid $grey400",
+      }
+    }
+  }
+});
