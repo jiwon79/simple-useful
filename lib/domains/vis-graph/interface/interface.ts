046c0eea8a0cea8093684ae00ac31776de198d3c
Lee Jiwon <jiiwon79@snu.ac.kr>
2023-10-28 20:56:05 +0900

    feat: domain-vis network 추가 (#13)
    * :sparkles: vis-network interface 추가
    
    * :sparkles: vis-network views 추가
    
    * :sparkles: vis-network useEdges 훅 추가

---
+++ lib/domains/vis-graph/interface/interface.ts
@@ -0,0 +1,11 @@
+export interface VisNetworkNode {
+  id: string;
+  label: string;
+  title: string;
+}
+
+export interface VisNetworkEdge {
+  from: string;
+  to: string;
+  color: string;
+}
---
+++ lib/domains/vis-graph/views/VisGraph.tsx
@@ -0,0 +1,40 @@
+import VisGraphWrapper, { Options } from "react-vis-graph-wrapper";
+import { VisNetworkEdge, VisNetworkNode } from "../interface"
+
+const options: Options = {
+  layout: {
+    // randomSeed: 0
+  },
+  nodes: {
+    borderWidth: 0,
+    shape: 'circularImage',
+    image: 'https://cdn-store.leagueoflegends.co.kr/images/v2/profileicons/4025.jpg',
+    scaling: {
+      customScalingFunction: function (min, max, total, value) {
+        return value / total;
+      },
+      min: 5,
+      max: 150,
+    },
+    font: {
+      size: 20,
+    },
+  },
+  edges: {
+    color: '#CCD1FF',
+  },
+};
+
+interface VisGraphProp {
+  nodes: VisNetworkNode[];
+  edges: VisNetworkEdge[];
+}
+
+export const VisGraph = ({nodes, edges}: VisGraphProp) => {
+  const graphData = {
+    nodes,
+    edges,
+  };
+
+  return (<VisGraphWrapper options={options} graph={graphData}/>)
+}
