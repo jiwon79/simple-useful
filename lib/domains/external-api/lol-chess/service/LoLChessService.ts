743cc82a0348228352dbe09dbf6537fc4e948f54
jiwon79 <19-079@ksa.hs.kr>
2023-10-29 18:18:02 +0900

    :sparkles: 최근 게임에서 Friend가져오는 기능 추가

--- lib/domains/external-api/lol-chess/service/LoLChessService.ts
+++ lib/domains/external-api/lol-chess/service/LoLChessService.ts
@@ -1,13 +1,20 @@
 import {
+  ExternalLoLChessMatch,
   ExternalLoLChessResponse,
+  ExternalLoLChessSummoner,
   LoLChessFriend,
 } from '@domains/external-api/lol-chess/interface';
+
+type SummonerMap = Map<string, ExternalLoLChessSummoner>;
+type MatchCountMap = Map<string, number>;
 
 export class LoLChessService {
   public async getLoLChessFriends(name: string): Promise<LoLChessFriend[]> {
     const externalResponse = await this.fetchExternalLoLChess(name);
+    const summonerMap = this.groupSummonerByPuuid(externalResponse.summoners);
+    const matchCountMap = this.getMatchCountMap(externalResponse.matches);
 
-    return [] as LoLChessFriend[];
+    return this.getFriendFromMap(matchCountMap, summonerMap);
   }
 
   private async fetchExternalLoLChess(
@@ -24,5 +31,51 @@ export class LoLChessService {
     const response = await fetch(externalRequestUrl).then((res) => res.json());
 
     return response as ExternalLoLChessResponse;
+  }
+
+  private groupSummonerByPuuid(
+    summoners: ExternalLoLChessSummoner[],
+  ): SummonerMap {
+    const summonerMap = new Map<string, ExternalLoLChessSummoner>();
+
+    summoners.forEach((summoner) => {
+      summonerMap.set(summoner.puuid, summoner);
+    });
+
+    return summonerMap;
+  }
+
+  private getMatchCountMap(matches: ExternalLoLChessMatch[]): MatchCountMap {
+    const matchCountMap = new Map<string, number>();
+
+    matches.forEach((match) => {
+      match.participants.forEach((participant) => {
+        const count = matchCountMap.get(participant.puuid) || 0;
+        matchCountMap.set(participant.puuid, count + 1);
+      });
+    });
+
+    return matchCountMap;
+  }
+
+  private getFriendFromMap(
+    matchCountMap: MatchCountMap,
+    summonerMap: SummonerMap,
+  ): LoLChessFriend[] {
+    const friends: LoLChessFriend[] = [];
+
+    matchCountMap.forEach((count, puuid) => {
+      const summoner = summonerMap.get(puuid);
+      if (!summoner || count < 2) {
+        return;
+      }
+
+      friends.push({
+        name: summoner.name,
+        count,
+      });
+    });
+
+    return friends;
   }
 }
